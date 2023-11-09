017a8985cc1aadf5bf7574a519592866fc103732
yogesh <yogeshgawali141@gmail.com>
2023-11-09 11:32:02 +0530

    Yogesh First Commit.

---
+++ SpringBoot-Runners/src/main/java/com/yogibaba/runners/MyFirstRunner.java
@@ -0,0 +1,13 @@
+package com.yogibaba.runners;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MyFirstRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Main execution logic
+
+	}
+
+}
